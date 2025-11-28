4616874a1ac5ea818ba3d7c8afd54b69dcdba9ac
park <park@gmail.com>
2025-11-28 15:11:01 +0900

    속도로 인한 벽 뚫기 해결방안

--- nuguri.c
+++ nuguri.c
@@ -253,15 +253,24 @@ void move_player(char input) {
 
             if (velocity_y < 0 && next_y < MAP_HEIGHT && map[stage][next_y][player_x] == '#') {
                 velocity_y = 0;
-            } else if (next_y < MAP_HEIGHT) {
-                player_y = next_y;
             }
-            
+	    else if (next_y < MAP_HEIGHT)
+	   if(velocity_y > 0) {
+		   for(int i =0; i<velocity_y; i++){
             if ((player_y + 1 < MAP_HEIGHT) && map[stage][player_y + 1][player_x] == '#') {
                 is_jumping = 0;
                 velocity_y = 0;
+		break;
             }
-        } else {
+	    player++;
+	      }
+	   }
+	   else{
+		   player_y = next_y;
+	   }
+	}
+    }
+        else {
             if (floor_tile != '#' && floor_tile != 'H') {
                  if (player_y + 1 < MAP_HEIGHT) player_y++;
                  else init_stage();
